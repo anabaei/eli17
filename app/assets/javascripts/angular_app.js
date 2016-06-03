1b6c6dd2e24e45883eb7d884813208b50fbb2fc3
Amir Nabaei <amir_nabaei@Amirs-MacBook-Air.local>
2016-06-03 15:10:47 -0700

    D

---
+++ app/assets/javascripts/angular_app.js
@@ -0,0 +1,8 @@
+
+// define which angular app is used
+var app = angular.module('myapp', []);
+// define a controller
+app.controller('myctr', function($scope)
+   {
+   	$scope.name = "Amir";
+   })
