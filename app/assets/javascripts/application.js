2bcf1d2d3a06f6fc83741ecdd6da2c0481e716c1
Amir Nabaei <amir_nabaei@Amirs-MacBook-Air.local>
2016-06-03 17:39:33 -0700

    DS

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -9,7 +9,8 @@
 //
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
-//
+
+//= require angular_app
 //= require jquery
 //= require jquery_ujs
 //= require turbolinks
